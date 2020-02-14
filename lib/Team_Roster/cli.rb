5779d81e552233b7b0026dceaba2f59d4f4e7084
Anthony Ybarra <anthony_jy72@yahoo.com>
2020-02-14 20:00:28 +0000

    third update

--- lib/Team_Roster/cli.rb
+++ lib/Team_Roster/cli.rb
@@ -1,12 +1,14 @@
 class TeamRoster::CLI
   
   def call
-    puts "Team Lineup"
     list_players
+    menu
+    goodbye
   end
   
   def list_players
-    puts <<-DOC
+    puts "Team Lineup:"
+    puts <<-DOC.gsub /^\s*/, ''
     1. Harryhook
     2. Unkoe
     3. AKM
@@ -18,4 +20,43 @@ class TeamRoster::CLI
     9. Trill
     10. Doha
     11. Crimzo
+    DOC
+  end
+  
+  def menu
+    
+    input = nil
+  while input != "exit"
+  puts "Enter the number of the player you want info on:"
+    input = gets.strip
+    case input
+      when "1"
+        puts "Info on Harryhook:"
+      when "2"
+        puts "Info on Unkoe:"
+      when "3"
+        puts "Info on AKM:"
+      when "4"
+        puts "Info on Gamsu:"
+      when "5"
+        puts "Info on Note:"
+      when "6"
+        puts "Info on Closer:"
+      when "7"
+        puts "Info on Decay:"
+      when "8"
+        puts "Info on Zachareee:"
+      when "9"
+        puts "Info on Trill:"
+      when "10"
+        puts "Info on Doha:"
+      when "11"
+        puts "Info on Crimzo:"
+      end
+    end
+  end
+  
+  def goodbye
+    puts "See you next time!"
+  end
 end
